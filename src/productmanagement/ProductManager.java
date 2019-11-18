a63f231ecb70ac5d1cedbd8bb19178bbc59bf987
jozhuatwx <jozhuatwx@gmail.com>
2019-11-18 13:34:44 +0800

    set active as default status

--- src/productmanagement/ProductManager.java
+++ src/productmanagement/ProductManager.java
@@ -3,6 +3,11 @@ package productmanagement;
 public class ProductManager extends User {
   private String status;
 
+  ProductManager(int userId, String userName, String userAddress, String userEmail, String userLoginName, String userPassword) {
+    super(userId, userName, userAddress, userEmail, "PM", userLoginName, userPassword);
+    this.status = "active";
+  }
+
   public String getStatus() {
     return status;
   }
@@ -11,7 +16,8 @@ public class ProductManager extends User {
     this.status = status;
   }
 
-  ProductManager(int userId, String userName, String userAddress, String userEmail, String userLoginName, String userPassword) {
-    super(userId, userName, userAddress, userEmail, "PM", userLoginName, userPassword);
+  @Override
+  public String toString() {
+    return String.valueOf(getUserId()) + "," + getUserName() + "," + getUserAddress() + "," + getUserEmail() + "," + getUserRole() + "," + getUserLoginName() + "," + getUserPassword() + "," + getStatus();
   }
 }
