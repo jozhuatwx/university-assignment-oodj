ae6b66b4e3d9b727b0944db2970c35a5d610dcab
jozhuatwx <jozhuatwx@gmail.com>
2019-11-18 13:29:54 +0800

    added status field to productmanager

--- src/productmanagement/ProductManager.java
+++ src/productmanagement/ProductManager.java
@@ -1,6 +1,16 @@
 package productmanagement;
 
 public class ProductManager extends User {
+  private String status;
+
+  public String getStatus() {
+    return status;
+  }
+  
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
   ProductManager(int userId, String userName, String userAddress, String userEmail, String userLoginName, String userPassword) {
     super(userId, userName, userAddress, userEmail, "PM", userLoginName, userPassword);
   }
