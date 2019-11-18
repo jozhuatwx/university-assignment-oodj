d8783238bc6f238ad3fbee2b6b81fb862d2bb97e
jozhuatwx <jozhuatwx@gmail.com>
2019-11-18 13:25:59 +0800

    fixed logging

--- src/productmanagement/ActionLog.java
+++ src/productmanagement/ActionLog.java
@@ -2,11 +2,11 @@ package productmanagement;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class ActionLog {
   public static void log(String action) {
-    String log = User.myAccount.getUserName();
-    // String log = String.valueOf(LocalDate.now()) + " " + String.valueOf(LocalTime.now()) + " " + String.valueOf(User.myAccount.getUserId()) + " " + User.myAccount.getUserRole() + " " + action;
+    String log = String.valueOf(LocalDate.now()) + " " + String.valueOf(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))) + " " + String.valueOf(User.myAccount.getUserId()) + " " + User.myAccount.getUserRole() + " " + action;
     WriteObject.write(log, "Log.txt", true);
   }
 }
