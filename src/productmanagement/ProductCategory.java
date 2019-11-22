22823c2b3faad8ddbcfb82d3ce891788a0781f45
jozhuatwx <jozhuatwx@gmail.com>
2019-11-22 14:37:56 +0800

    created productcategory class

---
+++ src/productmanagement/ProductCategory.java
@@ -0,0 +1,135 @@
+package productmanagement;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class ProductCategory {
+  private String categoryId, categoryName, categoryDescription;
+
+  // Construct the Product Category
+  ProductCategory(String categoryId, String categoryName, String categoryDescription) {
+    this.categoryId = categoryId;
+    this.categoryName = categoryName;
+    this.categoryDescription = categoryDescription;
+  }
+
+  // Getters and setters
+  /**
+   * @return the categoryId
+   */
+  public String getCategoryId() {
+    return categoryId;
+  }
+
+  /**
+   * @param categoryId the categoryId to set
+   */
+  public void setCategoryId(String categoryId) {
+    this.categoryId = categoryId;
+  }
+
+  /**
+   * @return the categoryName
+   */
+  public String getCategoryName() {
+    return categoryName;
+  }
+
+  /**
+   * @param categoryName the categoryName to set
+   */
+  public void setCategoryName(String categoryName) {
+    this.categoryName = categoryName;
+  }
+
+  /**
+   * @return the categoryDescription
+   */
+  public String getCategoryDescription() {
+    return categoryDescription;
+  }
+
+  /**
+   * @param categoryDescription the categoryDescription to set
+   */
+  public void setCategoryDescription(String categoryDescription) {
+    this.categoryDescription = categoryDescription;
+  }
+
+  // Generate the Product Category ID
+  public static String generateCategoryId() {
+    // Set default ID to 1
+    int intCategoryId = 1;
+    String categoryId = "S00000001";
+
+    try {
+      // Create a category array list
+      ArrayList<String> categoryArray = ReadObject.readArray("Category.txt");
+
+      if (categoryArray.size() > 0) {
+        // Get the last line of the category array list
+        String lastLine = categoryArray.get(categoryArray.size() - 1);
+        // Split the line into an array
+        String[] lastLineDetails = lastLine.split(",");
+        // Read the ID of the line and add by 1
+        intCategoryId = Integer.valueOf(lastLineDetails[0].substring(1)) + 1;
+        // Add 'CT' and leading zeros to the ID
+        categoryId = "CT" + String.format("%08d", intCategoryId);
+      }
+    } catch (FileNotFoundException e) {
+      // Ignore as there may be no exsting categorys
+    }
+    
+    // Return the value of the new Category ID
+    return categoryId;
+  }
+
+  public static void register(ProductCategory category) {
+    // Write the new category into the Category database
+    WriteObject.write(category, "Category.txt", true);
+    // Record the action into the log
+    ActionLog.log("Registered new Category (" + category.getCategoryId() + ")");
+  }
+
+  public static void update(ProductCategory category) {
+    int i = 0;
+    File oldFile = new File("Category.txt");
+    // Create a temporary file
+    File tempFile = new File("TempCategory.txt");
+
+    try {
+      // Create a category array list
+      ArrayList<String> categoryArray = ReadObject.readArray("Category.txt");
+      // Iterate through the category array
+      for (String categoryDetails : categoryArray) {
+        // Split line into array
+        String[] details = categoryDetails.split(",");
+        // Find the category with the matching ID
+        if (details[0].equals(category.getCategoryId())) {
+          // Write the new details into the temporary file
+          WriteObject.write(category, "TempCategory.txt", true);
+        } else {
+          // Write the old detail into the temporary file
+          WriteObject.write(categoryArray.get(i), "TempCategory.txt", true);
+        }
+        i++;
+      }
+      // Delete the old file
+      oldFile.delete();
+      // Rename the temporary file
+      tempFile.renameTo(new File("Category.txt"));
+      // Record the action into the log
+      ActionLog.log("Updated category information (" + category.getCategoryId() + ")");
+    } catch (FileNotFoundException e) {
+      // Display the error message
+      System.out.println(e);
+    }
+  }
+
+  // Overrides the default toString() to display the information of the Supplier class
+  @Override
+  public String toString() {
+    return String.valueOf(getCategoryId()) + "," + getCategoryName() + "," + getCategoryDescription();
+  }
+}
