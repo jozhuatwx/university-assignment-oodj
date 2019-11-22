bce94d604f400e7a783bb289c6ff7c7365a094ae
jozhuatwx <jozhuatwx@gmail.com>
2019-11-22 13:50:50 +0800

    added update test methods

--- src/productmanagement/Productmanagement.java
+++ src/productmanagement/Productmanagement.java
@@ -6,7 +6,7 @@ public class Productmanagement {
   }
 
   public static void testLogin() {
-    String userLoginName = "Admin";
+    String userLoginName = "admin";
     String userPassword = "adminpassword";
     
     User.login(userLoginName, userPassword);
@@ -20,8 +20,18 @@ public class Productmanagement {
     User.register(productManager);
   }
 
+  public static void testUserUpdate() {
+    ProductManager productManager = new ProductManager("U00000002", "UProductMan", "UProductManHome", "uproductman@mail.com", "uproductman", "uproductmanpassword", ProductManager.INACTIVE);
+    User.update(productManager);
+  }
+
   public static void testSupplierRegistration() {
     Supplier supplier = new Supplier(Supplier.generateSupplierId(), "IKEA", "Cheras", "ikea@gmail.com", "012-3456789", Supplier.ACTIVE);
     Supplier.register(supplier);
+  }
+
+  public static void testSupplierUpdate() {
+    Supplier supplier = new Supplier("S00000002", "UIKEA", "UCheras", "uikea@gmail.com", "012-3456789", Supplier.INACTIVE);
+    Supplier.update(supplier);
   }
 }
