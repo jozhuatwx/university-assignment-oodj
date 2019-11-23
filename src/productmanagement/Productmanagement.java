78a2ddfeecc1e3200d96c3678ed2fe811b8e529c
jozhuatwx <jozhuatwx@gmail.com>
2019-11-23 17:18:42 +0800

    updated test methods to encrypt password

--- src/productmanagement/Productmanagement.java
+++ src/productmanagement/Productmanagement.java
@@ -7,22 +7,36 @@ public class ProductManagement {
 
   public static void testLogin() {
     String userLoginName = "admin";
-    String userPassword = "adminpassword";
+    String userPasswordString = "adminpassword";
+    char[] userPassword = userPasswordString.toCharArray();
     
     User.login(userLoginName, userPassword);
   }
 
   public static void testUserRegistration() {
-    Administrator administrator = new Administrator(User.generateUserId(), "Admin", "AdminHome", "admin@mail.com", "admin", "adminpassword");
-    User.register(administrator);
+    try {
+      String adminPasswordString = "adminpassword";
+      Administrator administrator = new Administrator(User.generateUserId(), "Admin", "AdminHome", "admin@mail.com", "admin", Encryption.encryptPassword(adminPasswordString.toCharArray()));
+      User.register(administrator);
 
-    ProductManager productManager = new ProductManager(User.generateUserId(), "ProductMan", "ProductManHome", "productman@mail.com", "productman", "productmanpassword", ProductManager.ACTIVE);
-    User.register(productManager);
+      String productManPasswordString = "productmanpassword";
+      ProductManager productManager = new ProductManager(User.generateUserId(), "ProductMan", "ProductManHome", "productman@mail.com", "productman", Encryption.encryptPassword(productManPasswordString.toCharArray()), ProductManager.ACTIVE);
+      User.register(productManager);
+    } catch (Exception e) {
+      // Display the error message
+      System.out.println(e);
+    }
   }
 
   public static void testUserUpdate() {
-    ProductManager productManager = new ProductManager("U00000002", "UProductMan", "UProductManHome", "uproductman@mail.com", "uproductman", "uproductmanpassword", ProductManager.INACTIVE);
-    User.update(productManager);
+    try {
+      String productManPasswordString = "uproductmanpassword";
+      ProductManager productManager = new ProductManager("U00000002", "UProductMan", "UProductManHome", "uproductman@mail.com", "uproductman", Encryption.encryptPassword(productManPasswordString.toCharArray()), ProductManager.INACTIVE);
+      User.update(productManager);
+    } catch (Exception e) {
+      // Display the error message
+      System.out.println(e);
+    }
   }
 
   public static void testSupplierRegistration() {
