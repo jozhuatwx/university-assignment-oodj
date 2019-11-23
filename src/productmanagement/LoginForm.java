5da16b9274ecc9cea5083385de243ab088e2c863
jozhuatwx <jozhuatwx@gmail.com>
2019-11-23 20:31:31 +0800

    cleared unnecessary comment

--- src/productmanagement/LoginForm.java
+++ src/productmanagement/LoginForm.java
@@ -1,24 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package productmanagement;
 
 import java.awt.Color;
 import javax.swing.JFrame;
 
-/**
- *
- * @author User
- */
 public class LoginForm extends javax.swing.JFrame {
-
-    /**
-     * Creates new form Login
-     */
-    
-    // For the mouse drag event
     int xMouse, yMouse;
     
     public LoginForm() {
