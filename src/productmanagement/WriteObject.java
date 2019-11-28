48123b9d4d6d5dcc6ebcaa1a9a377a48312f8220
jozhuatwx <jozhuatwx@gmail.com>
2019-11-28 21:48:23 +0800

    updated writeobject class to display success message

--- src/productmanagement/WriteObject.java
+++ src/productmanagement/WriteObject.java
@@ -9,35 +9,47 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class WriteObject {
-  public static void write(Object obj, String filename, boolean append) {
+  public static boolean write(Object obj, String filename, boolean append) {
     // Writes onto the file based on the path and whether to append or overwrite it
     try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, append))) {
-      // Writes the String of the object and ends 
+      // Writes the String of the object and ends
       writer.write(String.valueOf(obj));
       // Creates a new line in the file
       writer.newLine();
       // Clears the resources in writer
       writer.close();
+      return true;
     } catch (Exception e) {
       // Displays the error message
       JOptionPane.showMessageDialog(new JFrame(), e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
     }
+    return false;
   }
 
-  public static void log(String action) {
+  public static boolean log(String action) {
     // Get the date and time in the format of YYYY-MM-dd HH:mm:ss
     String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss"));
     // Creates a log with the date, time, user ID, user role and the action made
     String log = dateTime + ";" + User.myUser.getUserId() + ";" + User.myUser.getUserRole() + ";" + action;
     // Writes the log into the file
-    write(log, "Log.txt", true);
+    return write(log, "Log.txt", true);
   }
 
-  public static void write(Object obj, String filename, boolean append, String action) {
+  public static void write(Object obj, String fileName, boolean append, String action) {
+    // Default to not show message
+    write(obj, fileName, append, action, false);
+  }
+
+  public static void write(Object obj, String fileName, boolean append, String action, boolean displayMessage) {
     // Writes original file
-    write(obj, filename, append);
+    write(obj, fileName, append);
 
     // Writes actions into log
     log(action);
+
+    if (displayMessage) {
+      // Display the success message
+      JOptionPane.showMessageDialog(new JFrame(), action, "Success", JOptionPane.INFORMATION_MESSAGE);
+    }
   }
 }
