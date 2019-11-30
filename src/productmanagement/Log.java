5756113a4ef05733aacf87fef77d7b7cf4a04938
jozhuatwx <jozhuatwx@gmail.com>
2019-11-30 17:06:39 +0800

    updated log class

--- src/productmanagement/Log.java
+++ src/productmanagement/Log.java
@@ -1,18 +1,74 @@
 package productmanagement;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Log {
   // Constant fields
-  public final static String LOG_FILE_NAME = "Log.txt";
+  public final static String FILE_NAME = "Log.txt";
+
+  // Log fields
+  private LocalDateTime logDateTime;
+  private String logUserId, logUserRole, logAction;
+
+  Log(LocalDateTime logDateTime, String logUserId, String logUserRole, String logAction) {
+    this.logDateTime = logDateTime;
+    this.logUserId = logUserId;
+    this.logUserRole = logUserRole;
+    this.logAction = logAction;
+  }
+  
+  Log(String[] details) {
+    this(LocalDateTime.parse(details[0]), details[1], details[2], details[3]);
+  }
+
+  // Getters and setters
+  public LocalDateTime getLogDateTime() {
+    return logDateTime;
+  }
+
+  public String getLogUserId() {
+    return logUserId;
+  }
+
+  public String getLogUserRole() {
+    return logUserRole;
+  }
+  
+  public String getLogAction() {
+    return logAction;
+  }
 
   public static boolean write(String action) {
-    // Get the date and time in the format of YYYY-MM-dd HH:mm:ss
-    String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss"));
     // Creates a log with the date, time, user ID, user role and the action made
-    String log = dateTime + ";" + User.myUser.getUserId() + ";" + User.myUser.getUserRole() + ";" + action;
+    String log = LocalDateTime.now() + ";" + User.myUser.getUserId() + ";" + User.myUser.getUserRole() + ";" + action;
     // Writes the log into the file
-    return WriteObject.write(log, LOG_FILE_NAME, true);
+    return WriteObject.write(log, FILE_NAME, true);
+  }
+
+  public static ArrayList<Log> search(String keyword) {
+    keyword = keyword.toLowerCase();
+    ArrayList<Log> logArrayList = new ArrayList<>();
+
+    try {
+      ArrayList<String> logArray = ReadObject.readArray(FILE_NAME);
+      // Iterate through the Log array
+      for (String logDetails : logArray) {
+        // Split the line into an array
+        String[] details = logDetails.split(";");
+        // Find if any existing Log matches the keyword
+        Log log = new Log(details);
+        if (String.valueOf(log.getLogDateTime()).toLowerCase().contains(keyword) || log.getLogUserId().toLowerCase().contains(keyword) || log.getLogAction().toLowerCase().contains(keyword)) {
+          logArrayList.add(log);
+        }
+      }
+    } catch (Exception e) {
+      // Display the error message
+      JOptionPane.showMessageDialog(new JFrame(), e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+    }
+    return logArrayList;
   }
 }
